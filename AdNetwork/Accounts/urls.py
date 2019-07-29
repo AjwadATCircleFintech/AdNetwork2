54a5d67d0bbc845d847708eae1750ae5a2245990
Ajwad@Circle <i-ajwad@circlefintech.com>
2019-07-29 11:58:50 +0600

    authentication function isn't working and css is a little fucked, focus on auth and demo first

--- AdNetwork/Accounts/urls.py
+++ AdNetwork/Accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('accounts/login_process',views.login_process, name='loginprocess'), # try to change this, once up fix view functions
+   # path('accounts/login_process',views.login_process, name='loginprocess'), # try to change this, once up fix view functions
     path('accounts/signup', views.signup, name='signup'),
-    path('accounts/confirmation', views.confirmation, name='confirmation'),
+    #path('accounts/confirmation', views.confirmation, name='confirmation'),
     path('accounts/recovery', views.recovery, name='recovery'),
 ]
--- AdNetwork/Accounts/views.py
+++ AdNetwork/Accounts/views.py
@@ -5,48 +5,52 @@ from django.contrib.auth import authenticate
 
 #Todo find out why changing space makes the code not work
 # Create your views here.
-def login(request):
-    # Confirm with authenticate
 
-    return render(request, 'accounts/login.html')
+def login(request):
+
+    if request.method == 'POST':
+        username = request.POST['Username']
+        password = request.POST['Password']
+        print(username)
+        print(password)
+        user = authenticate(email=username, password=password)
+        print(user)
+        if user is not None:
+            print("This got triggered")
+            return render(request, 'accounts/confirmation.html')
+        else:
+            print("No this one got triggered")
+            return render(request, 'accounts/login.html')
+
+    else:
+
+        return render(request, 'accounts/login.html')
 
 
 def signup(request): # clean this up later
 
+    if request.method == 'POST':
+        username = request.POST['username']
+        fist_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        re_password = request.POST['re_password']
 
+        if password == re_password:
+           NewUser = Accounts.objects.create_user(username=username, password=password, email=email, first_name=fist_name, last_name=last_name)
+           print(NewUser)
+           return redirect('login')
+        else:
+            return redirect('signup')
 
-    return render(request, 'accounts/signup.html')
-
-def login_process(request):
-
-    #We need a validator for this
-
-    username = request.POST['username']
-    fist_name = request.POST['first_name']
-    last_name = request.POST['last_name']
-    email = request.POST['email']
-    password = request.POST['password']
-    re_password = request.POST['re_password']
-
-    if password == re_password:
-        Accounts.objects.create_user(username=username, password=password, email=email, first_name=fist_name, last_name=last_name)
-        return render(request,'accounts/confirmation.html')
     else:
-        return redirect('/')
 
-def confirmation(request):
-    username = request.POST['Username']
-    password = request.POST['Password']
-    print(username)
-    print(password)
-    user = authenticate(username=username,password=password)
-    print(user)
-    if user is not None:
-        print("This got triggered")
-        return render(request,'accounts/confirmation.html')
-    else:
-        print("No this one got triggered")
-        return render(request,'accounts/login.html')
+        return render(request, 'accounts/signup.html')
+
+
+
+
 
 def recovery(request):
 
